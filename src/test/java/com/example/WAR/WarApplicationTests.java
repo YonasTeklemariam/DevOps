e9436d8b30ee54c80afe5e5e31f403afa6bdffc3
YonasTeklemariam <74572394+YonasTeklemariam@users.noreply.github.com>
2021-08-27 13:34:40 -0500

    Add files via upload

---
+++ src/test/java/com/example/WAR/WarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
